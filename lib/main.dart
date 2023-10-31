440070d859975dfcdcd35c71dd408e0761245241
vinayak5002 <82216732+vinayak5002@users.noreply.github.com>
2023-10-31 15:05:43 +0530

    added firebase_options

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'Data/Data.dart';
 import 'Pages/MainPage.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  
   runApp(const MyApp());
 }
 
