95c3d1ad15a1bffb1cd0cbfb3002ba72c1da468e
vinayak5002 <82216732+vinayak5002@users.noreply.github.com>
2023-11-03 14:32:30 +0530

    Fixed connection to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 import 'Data/Data.dart';
 import 'Pages/MainPage.dart';
 
-Future main() async {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {  
+
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
